308ba9589f70910ee1c6072e3b6f2a9afbe89e24
jushinpon <jushinpon@gmail.com>
2021-03-05 15:33:39 +0800

    parallel version for manager script

--- ssh_install.pl
+++ ssh_install.pl
@@ -1,6 +1,12 @@
-for (1..2){
+use Parallel::ForkManager;
+$forkNo = 30;
+my $pm = Parallel::ForkManager->new("$forkNo");
+
+for (1..10){
+    $pm->start and next;
     $nodeindex=sprintf("%02d",$_);
     $nodename= "node"."$nodeindex";
-    system("ssh $nodename 'shutdown -h now'");
-
+    system("ssh $nodename 'dnf install perl-Statistics-Descriptive -y'");
+    $pm->finish;
 }
+$pm->wait_all_children;
