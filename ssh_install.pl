6ea6a22272b9d12fa4c1f9b2fa49a471dc282c3c
jushinpon <jushinpon@gmail.com>
2021-03-13 02:05:05 +0800

    gtk* for sview

--- ssh_install.pl
+++ ssh_install.pl
@@ -2,11 +2,14 @@ use Parallel::ForkManager;
 $forkNo = 30;
 my $pm = Parallel::ForkManager->new("$forkNo");
 
-for (1..10){
-    $pm->start and next;
+for (1..41){
+    #$pm->start and next;
     $nodeindex=sprintf("%02d",$_);
     $nodename= "node"."$nodeindex";
-    system("ssh $nodename 'dnf install perl-Statistics-Descriptive -y'");
-    $pm->finish;
+    #system("ssh $nodename 'dnf install perl-Statistics-Descriptive -y'");
+    print "nodename:$nodename\n";
+    system("munge -n \| ssh $nodename unmunge");
+    if($!){sleep(3);}
+    #$pm->finish;
 }
-$pm->wait_all_children;
+#$pm->wait_all_children;
