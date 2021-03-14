f56d378a2f35c692b17ddc5695f4d238c21caf34
jushinpon <jushinpon@gmail.com>
2021-03-15 00:52:06 +0800

    modify SambaInstall.pl

--- packages/SambaInstall.pl
+++ packages/SambaInstall.pl
@@ -1,15 +1,18 @@
 system ("systemctl stop smb");
 system("yum install samba -y");
+system("firewall-cmd --zone=external --add-port=139/tcp --permanent"); # for samba
+system ("firewall-cmd --reload"); #reload
+
 $ConfPath = '/etc/samba/smb.conf';
 system("rm $ConfPath");
 system("touch $ConfPath");
 
-$filename = 'home';
+$filename = 'jsp_only';
 $filepath = '/home/jsp';
 $description = 'Shared'; 
 $browseable = 'yes';
-$readonly = 'no';
-$Autority = '777';
+$readonly = 'yes';
+$Authority = '755';
 $UserList = 'jsp';
 #$hostallow = '140.117.59.184'; #maybe u don't need this because u have firewall,right?
 @UserList = ("jsp");
@@ -19,13 +22,14 @@ $UserList = 'jsp';
 `echo "	path = $filepath" >> $ConfPath`;
 `echo "	browseable = $browseable" >> $ConfPath`;
 `echo "	writable = $browseable" >> $ConfPath`;
-`echo "	create mask = $Autority" >> $ConfPath`;
-`echo "	directory mask = $Autority" >> $ConfPath`;
+`echo "	create mask = $Authority" >> $ConfPath`;
+`echo "	directory mask = $Authority" >> $ConfPath`;
 `echo "	valid users = $UserList" >> $ConfPath`;
+`echo "	read only = Yes" >> $ConfPath`;
 
 foreach (@UserList)
 	{
-		system ("pdbedit -a -u $_"); #you need to set passwd for samba by hand 
+		system ("echo '123'|pdbedit -a -u $_"); #you need to set passwd for samba by hand 
 	}
 
 `echo '
@@ -35,18 +39,7 @@ foreach (@UserList)
 	server string = SAMBA SERVER	 
 	security = user' >> $ConfPath
 	`;
-system("testparm");#for test the install process done or not 
+system("echo -e '\n'| testparm > smbCheck.txt");#for test the install process done or not 
 system ("systemctl start smb");
 system ("systemctl enable smb");
 system ("setsebool -P samba_enable_home_dirs on");
-
-##DetailSet
-=beg
-
-hosts allow = $hostallow;
-max connections = 10;
-systemclt restart smb;
-
-=cut
-
-
